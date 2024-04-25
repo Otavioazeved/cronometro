8018c5efbbc842ba5ea157423c0f7d8c91a2123a
otavio azevedo <otavioazevedo39@gmail.com>
2024-04-25 15:48:38 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,3 +1,46 @@
+var horas = 0
+var sec = 0
+var min = 0
+var interval 
+
+function twoDigits(digit){
+    if(digit < 10){
+        return ('0'+ digit)
+    }else {
+        return (digit)
+    }
+}
+
+
+
 function start () {
-    console.log("iniciou")
+    cronometro()
+  interval = setInterval(cronometro, 1)
+
+}
+
+
+
+function pause() {
+    clearInterval(interval)
+}
+
+function stop() {
+ clearInterval(interval)
+ sec=0
+ min=0
+ document.getElementById("relogio").innerHTML = '00:00'
+}
+
+
+function cronometro (){
+    sec++
+    if(sec==60){
+        min++
+        sec==0
+        if(min==60)
+        min==0
+        horas++
+    }
+    document.getElementById("relogio").innerText=twoDigits(horas)+':'+twoDigits(min)+ ':'+sec+twoDigits(sec)
 }
